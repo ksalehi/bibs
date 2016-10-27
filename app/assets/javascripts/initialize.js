ad8129bcc80b4e7208525dbf6e2b1ce4b4b0fcc4
mbr84 <matthew.bramfeld@gmail.com>
2016-10-27 16:48:11 -0700

    application.js application.css and initialize.js

---
+++ app/assets/javascripts/initialize.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef*/
+
+$(document).ready(() => {
+  $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
